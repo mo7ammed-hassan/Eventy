5bd4cd62e4bb12d50f0714eeba3a478167bcdb48
Mohamed Hassan <mohamed7github@gmail.com>
2025-07-07 04:51:22 +0300

    - apply ontap in trending and upcoming event card to show in details screen

--- lib/features/home/presentation/widgets/trending_section/trending_event_card.dart
+++ lib/features/home/presentation/widgets/trending_section/trending_event_card.dart
@@ -1,10 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:eventy/config/routing/routes.dart';
 import 'package:eventy/config/service_locator.dart';
 import 'package:eventy/core/constants/app_colors.dart';
 import 'package:eventy/core/constants/app_images.dart';
 import 'package:eventy/core/constants/app_sizes.dart';
 import 'package:eventy/core/constants/app_styles.dart';
 import 'package:eventy/core/utils/device/device_utils.dart';
+import 'package:eventy/core/utils/helpers/extensions/navigation_extension.dart';
 import 'package:eventy/core/utils/helpers/helper_functions.dart';
 import 'package:eventy/core/widgets/shimmer/shimmer_widget.dart';
 import 'package:eventy/features/user_events/domain/entities/event_entity.dart';
@@ -20,91 +22,97 @@ class TrendingEventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = HelperFunctions.isDarkMode(context);
-    return Container(
-      width: DeviceUtils.getScaledWidth(context, 0.8),
-      decoration: _buildCardDecoration(isDark),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          final imageHight = constraints.maxHeight * 0.67;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // -- TrendingEventCard Header
-              // -- Image
-              SizedBox(
-                height: imageHight,
-                // -- Top widgets
-                child: Stack(
-                  children: [
-                    // -- Event Image
-                    _EventImageSection(event?.image),
-                    // -- Top ribbon
-                    Positioned.fill(
-                      child: _TopRibbon(event ?? EventEntity.empty()),
-                    ),
-                  ],
-                ),
-              ),
-
-              // -- TrendingEventCard Body
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10.0,
-                  left: 10.0,
-                  right: 10.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // -- Event title
-                    Text(
-                      '${event?.name} / ${event?.formattedDate}',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
+    return GestureDetector(
+      onTap: () =>
+          context.pushNamedPage(Routes.eventDetailsScreen, arguments: event),
+      child: Container(
+        width: DeviceUtils.getScaledWidth(context, 0.8),
+        decoration: _buildCardDecoration(isDark),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final imageHight = constraints.maxHeight * 0.67;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // -- TrendingEventCard Header
+                // -- Image
+                SizedBox(
+                  height: imageHight,
+                  // -- Top widgets
+                  child: Stack(
+                    children: [
+                      // -- Event Image
+                      _EventImageSection(event?.image),
+                      // -- Top ribbon
+                      Positioned.fill(
+                        child: _TopRibbon(event ?? EventEntity.empty()),
                       ),
-                    ),
+                    ],
+                  ),
+                ),
 
-                    // -- Event Price
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          event?.paid == true ? 'Rs {${event?.price}}' : 'Free',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context).textTheme.bodySmall
-                              ?.copyWith(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                              ),
+                // -- TrendingEventCard Body
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    left: 10.0,
+                    right: 10.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // -- Event title
+                      Text(
+                        '${event?.name} / ${event?.formattedDate}',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
                         ),
                       ),
-                    ),
 
-                    // -- User whos join
-                    AttendeeAvatars(
-                      avatarSize: 10,
-                      width: 60,
-                      fntSize: 12,
-                      attendees: [
-                        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D',
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5j8nUEqIX1fJuWCjZxWDh1rL-QL_cq2A-85035phw9d-hiWbpU7r6H2WKRJ2spHwcJGE&usqp=CAU',
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmx-wxle_unpBUp-PdatrfcHp3ljhBkIHdLeEmYmn6CYmJrpMAzOVfUxCu9CKX19zxsqA&usqp=CAU',
-                      ],
-                    ),
-                  ],
+                      // -- Event Price
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text(
+                            event?.paid == true
+                                ? 'Rs {${event?.price}}'
+                                : 'Free',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.bodySmall
+                                ?.copyWith(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                          ),
+                        ),
+                      ),
+
+                      // -- User whos join
+                      AttendeeAvatars(
+                        avatarSize: 10,
+                        width: 60,
+                        fntSize: 12,
+                        attendees: [
+                          'https://images.unsplash.com/photo-1633332755192-727a05c4013d?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D',
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5j8nUEqIX1fJuWCjZxWDh1rL-QL_cq2A-85035phw9d-hiWbpU7r6H2WKRJ2spHwcJGE&usqp=CAU',
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmx-wxle_unpBUp-PdatrfcHp3ljhBkIHdLeEmYmn6CYmJrpMAzOVfUxCu9CKX19zxsqA&usqp=CAU',
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
--- lib/features/home/presentation/widgets/upcoming_section/upcoming_event_card.dart
+++ lib/features/home/presentation/widgets/upcoming_section/upcoming_event_card.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:eventy/config/routing/routes.dart';
 import 'package:eventy/core/constants/app_colors.dart';
 import 'package:eventy/core/constants/app_sizes.dart';
 import 'package:eventy/core/constants/app_styles.dart';
+import 'package:eventy/core/utils/helpers/extensions/navigation_extension.dart';
 import 'package:eventy/core/utils/helpers/helper_functions.dart';
 import 'package:eventy/core/widgets/shimmer/shimmer_widget.dart';
 import 'package:eventy/features/user_events/domain/entities/event_entity.dart';
@@ -18,80 +20,88 @@ class UpcomingEventCard extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         final imageHight = constraints.maxHeight * 0.66;
-        return Container(
-          decoration: BoxDecoration(
-            color: isDark ? AppColors.dark : Colors.white,
-            borderRadius: BorderRadius.circular(AppSizes.eventCardRadius),
-            boxShadow: AppStyles.eventCardShadow(isDark),
+        return GestureDetector(
+          onTap: () => context.pushNamedPage(
+            Routes.eventDetailsScreen,
+            arguments: event,
           ),
-          child: Column(
-            children: [
-              // -- Event Image
-              Flexible(
-                flex: 2,
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: AppStyles.eventCardRadius,
-                      child: CachedNetworkImage(
-                        imageUrl: event.image,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                        height: imageHight,
-                        placeholder: (context, url) => const ShimmerWidget(),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      ),
-                    ),
-
-                    // -- Top ribbon
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: IconButton(
-                        icon: const Icon(
-                          Iconsax.archive_1,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          child: Container(
+            decoration: BoxDecoration(
+              color: isDark ? AppColors.dark : Colors.white,
+              borderRadius: BorderRadius.circular(AppSizes.eventCardRadius),
+              boxShadow: AppStyles.eventCardShadow(isDark),
+            ),
+            child: Column(
+              children: [
+                // -- Event Image
+                Flexible(
+                  flex: 2,
+                  child: Stack(
                     children: [
-                      // -- Event title
-                      Text(
-                        event.name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 13.0,
+                      ClipRRect(
+                        borderRadius: AppStyles.eventCardRadius,
+                        child: CachedNetworkImage(
+                          imageUrl: event.image,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: imageHight,
+                          placeholder: (context, url) => const ShimmerWidget(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
                         ),
                       ),
 
-                      // -- Event Price
-                      Text(
-                        event.description,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          fontSize: 12.0,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w600,
+                      // -- Top ribbon
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: const Icon(
+                            Iconsax.archive_1,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // -- Event title
+                        Text(
+                          event.name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodyMedium!
+                              .copyWith(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 13.0,
+                              ),
+                        ),
+
+                        // -- Event Price
+                        Text(
+                          event.description,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodySmall
+                              ?.copyWith(
+                                fontSize: 12.0,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
